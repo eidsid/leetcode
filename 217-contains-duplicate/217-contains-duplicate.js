c7b7ddb145020fc1fb936f7b78aa7a97847973a6
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-05-21 21:19:52 +0200

    Time: 884 ms (13.38%), Space: 48.2 MB (97.80%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,11 +2,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function (nums, memo = {}, counter = 0) {
-  if (nums.length === 0) return false;
-  if (nums[0] in memo) return true;
 
-  memo[nums.shift()] = "";
-
-  return containsDuplicate(nums, memo);
+var containsDuplicate = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] == nums[i]) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
