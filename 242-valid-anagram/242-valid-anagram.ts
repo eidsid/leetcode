79ea19d52a7c0ce35380ec422ac476d662dacd53
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-06-05 14:35:10 +0200

    Time: 171 ms (13.89%), Space: 45.7 MB (46.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.ts
+++ 242-valid-anagram/242-valid-anagram.ts
@@ -1,9 +1,23 @@
-function isAnagram(s: string, t: string): boolean {
+function isAnagram(s: string, t: string): boolean { 
+  let sobj = {};
+  let tobj = {};
+  let equal = true;
+  if (s.length === t.length) {
+    for (let i = 0; i < s.length; i++) {
+      if (typeof sobj[s[i]] == "undefined") sobj[s[i]] = 0;
+      if (typeof tobj[t[i]] == "undefined") tobj[t[i]] = 0;
+      sobj[s[i]] = sobj[s[i]] += 1;
+      tobj[t[i]] = tobj[t[i]] += 1;
+    }
 
-  if(s.length!==t.length) return false
-
-  let sorteds=[...s].sort().join('')
-  let sortedt=[...t].sort().join('')
-return  sorteds==sortedt
-  
+    Object.entries(sobj).forEach(([key, value]) => {
+      if (tobj[key] !== value) {
+        equal = false;
+        return;
+      }
+    });
+    return equal;
+  } else {
+    return false;
+  }
 };
