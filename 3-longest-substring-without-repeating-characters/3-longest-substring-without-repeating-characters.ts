a22f44c5e510d0b77eb60c2d718301b94b532dfc
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-06-09 13:50:41 +0200

    Time: 400 ms (21.65%), Space: 49.2 MB (45.16%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
@@ -1,13 +1,14 @@
 function lengthOfLongestSubstring(s: string): number {
   let LongestSubStringLength: number = 0
+      let currentString = ""
+
   for (let i = 0; i < s.length; i++) {
-    let currentString = ""
-   
+currentString=""
     for (let j = i; j < s.length; j++) {
 
       if (!currentString.includes(s[j])) {
         currentString += s[j]
-      
+
         if (LongestSubStringLength <= currentString.length) {
           LongestSubStringLength = currentString.length;
         }
