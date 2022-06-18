21c894a8e2c7d6110790c9536c82a5b866404958
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-06-18 10:45:50 +0200

    Time: 143 ms (68.51%), Space: 50.1 MB (12.92%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.ts
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.ts
@@ -1,25 +1,5 @@
 function sortedSquares(nums: number[]): number[] {
-  let results: number[] = [];
-
- 
-for (let i = 0; i < nums.length; i++) {
-    
-      let value = nums[i] * nums[i]
-
-    if (results[Math.floor(results.length / 2)] < value) 
-    
-    {
-      results.push(value)
-
-   
-    } else {
-    
-        results.unshift(value)
-    
-    }
-  
-  }
-
-  
-    return results.sort((a, b) => a - b)
+  nums.sort((a, b) => (a * a) - (b * b))
+  let mapnum = nums.map(x => x * x)
+  return mapnum
 };
