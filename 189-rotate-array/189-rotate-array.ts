0363b63fdc74e86fe530fac60f9c5e9949bc18cf
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-06-19 11:35:15 +0200

    Time: 175 ms (57.30%), Space: 51.8 MB (68.73%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.ts
@@ -0,0 +1,18 @@
+function rotate(nums: number[], k: number): void {
+
+  if (nums.length === k) { }
+  else if (nums.length > k) {
+    let rotatedPort = nums.splice(-k)
+    nums.unshift(...rotatedPort)
+  } else {
+    let resultArray: number[] = []
+    for (let i = 0; i <= nums.length - 1; i++) {
+      let numidex = (i + k) % nums.length;
+      resultArray[numidex] = nums[i]
+    }
+    for (let j = 0; j < resultArray.length; j++) {
+      nums[j] = resultArray[j];
+    }
+  }
+
+};
