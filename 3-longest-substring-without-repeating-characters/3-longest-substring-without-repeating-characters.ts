095de252cd39a844c24cc4715cc1b9890ac748c2
Eid Sayed Taha <65725990+eidsid@users.noreply.github.com>
2022-06-22 19:05:04 +0200

    Time: 520 ms (15.75%), Space: 49.6 MB (33.02%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
@@ -1,10 +1,8 @@
 function lengthOfLongestSubstring(s: string): number {
   let LongestSubStringLength: number = 0;
   let text: string = "";
-  let possibleLongswap = true;
   for (let i = 0; i < s.length; i++) {
     text = "";
-    if (!possibleLongswap) break;
     for (let j = i; j < s.length; j++) {
       if (!text.includes(s[j])) {
         text += s[j];
@@ -12,7 +10,6 @@ function lengthOfLongestSubstring(s: string): number {
           LongestSubStringLength = text.length;
         }
       } else {
-        if (s.length - i < text.length) possibleLongswap =false;
         break;
       }
     }
